08b5a0fa52615a3aecd16d5ebfca7f40e491db34
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-06-03 20:00:19 -0400

    updates from other projects

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: SQL",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
     ]
 )
