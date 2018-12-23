a5a93c89a9e68637f622fbc23b8604045d7ac24a
Victoria Lin <xilin@cs.washington.edu>
2018-12-22 21:39:08 -0800

    add formatting function for NOT LIKE operator

--- moz_sql_parser/formatting.py
+++ moz_sql_parser/formatting.py
@@ -154,6 +154,9 @@ class Formatter:
     def _like(self, pair):
         return '{0} LIKE {1}'.format(self.dispatch(pair[0]), self.dispatch(pair[1]))
 
+    def _notlike(self, pair):
+        return '{0} NOT LIKE {1}'.format(self.dispatch(pair[0]), self.dispatch(pair[1]))
+
     def _is(self, pair):
         return '{0} IS {1}'.format(self.dispatch(pair[0]), self.dispatch(pair[1]))
 
