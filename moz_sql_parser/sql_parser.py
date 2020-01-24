3d0651221f78281060a2a3218ccd7125c5b182a1
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-01-23 21:22:24 -0500

    update version number

--- moz_sql_parser/sql_parser.py
+++ moz_sql_parser/sql_parser.py
@@ -24,7 +24,7 @@ from moz_sql_parser.keywords import AND, AS, ASC, BETWEEN, CASE, COLLATE_NOCASE,
 ParserElement.enablePackrat()
 
 # PYPARSING USES A LOT OF STACK SPACE
-sys.setrecursionlimit(2000)
+sys.setrecursionlimit(1500)
 
 IDENT_FIRST_CHAR = alphas + "_"
 IDENT_REST_CHAR = alphanums + "_$"
