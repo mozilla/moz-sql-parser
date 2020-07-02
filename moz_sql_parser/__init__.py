3cccb8a04bac942391921cfe45502842ab87c9bf
Reuven Kaplan <rkaplan18@gmail.com>
2020-07-02 18:08:24 +0300

    Import mapping from collections.abc
    Fixing this warning:  ...\moz_sql_parser\__init__.py:12: DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3,and in 3.9 it will stop working
        from collections import Mapping

--- moz_sql_parser/__init__.py
+++ moz_sql_parser/__init__.py
@@ -9,7 +9,7 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
-from collections import Mapping
+from collections.abc import Mapping
 import json
 from threading import Lock
 
