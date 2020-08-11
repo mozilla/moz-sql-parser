292fb8dea605e225d5c6baabd7a83c9b0dc0f1c2
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-08-11 07:15:56 -0400

    minor change

--- moz_sql_parser/sql_parser.py
+++ moz_sql_parser/sql_parser.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
 #
 
 from __future__ import absolute_import, division, unicode_literals
