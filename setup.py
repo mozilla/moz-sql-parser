e56b326b0d4d5e576eca7d21c6fc47805461b3fe
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-10-16 19:44:31 -0400

    add dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description_content_type=u'text/markdown',
     include_package_data=True,
     classifiers=["Development Status :: 3 - Alpha","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","Programming Language :: SQL","Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.6","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"],
-    install_requires=["mo-future>=3.31.20024"],
+    install_requires=["mo-future", "mo-dots", "mo-logs"],
     version=u'3.32.20026',
     url=u'https://github.com/mozilla/moz-sql-parser',
     zip_safe=True,
