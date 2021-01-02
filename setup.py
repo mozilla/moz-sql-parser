d60a9d58074aade3c543769007f1704717399550
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-01-02 17:20:58 -0500

    update version number

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     name='moz-sql-parser',
     packages=["moz_sql_parser","mo_parsing"],
     url='https://github.com/klahnakoski/moz-sql-parser',
-    version='4.8.21002',
+    version='4.9.21002',
     zip_safe=True
 )
