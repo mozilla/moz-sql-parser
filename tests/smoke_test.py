2bbde768854f01c051ee9bcc2bc2ee88b83e340b
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-01-26 19:54:41 -0500

    updates from other projects

---
+++ tests/smoke_test.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+from moz_sql_parser import parse
+
+parse("select ")
